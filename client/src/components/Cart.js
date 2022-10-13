39c0407cbd05cba50515de5a0935f42b0f121fb3
cyron arieri <ariericyron@gmail.com>
2022-10-13 21:36:10 +0300

    created the cart component

---
+++ client/src/components/Cart.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+function Cart() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getCartItems();
+  }, []);
+
+  // delete item from cart
+  async function handleDelete(id) {
+    let result = await fetch(`http://localhost:4000/carts/${id}`, {
+      method: "DELETE",
+    });
+    result = await result.json();
+    console.warn(result);
+    getCartItems();
+  }
+
+  async function getCartItems() {
+    let result = await fetch("http://localhost:4000/carts");
+    result = await result.json();
+    setItems(result);
+  }
+
+  return (
+    <div className="container-card">
+      <h1>Cart Items</h1>
+      {items.map((item) => {
+        return (
+          <div className="card-container" key={item.id}>
+            <div className="card-cart">
+              <img src={item.image_url} alt={item.title}></img>
+              <div>
+                <h2>{item.title}</h2>
+                <h3>{item.price} KES</h3>
+              </div>
+              <button onClick={() => handleDelete(item.id)}>Remove Item</button>
+            </div>
+            <br></br>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default Cart;
